f5292ee446b5aaca1ea326a15a8d508da61c4703
Sam Bailey <samcbailey90@gmail.com>
2020-09-12 18:14:18 +0200

    Started building tests

--- tests/tyme/utils/test_timeseries.py
+++ tests/tyme/utils/test_timeseries.py
@@ -1,35 +1,60 @@
 import numpy as np
 import pandas as pd
+import pytest
+
 from tyme.utils import GroupedTimeSeries
 
 
-def test_timeseries_regression():
-    input_df = pd.DataFrame({
-        "group_a": ["a"]*15 + ["b"]*15,
-        "group_b": [1,2]*15,
-        "yyyy_mm_dd": [f"2020-01-{d:02}" for d in range(1, 31)],
-        "y": list(range(30))
-    })
+@pytest.fixture()
+def simple_a_b_input_df():
+    return pd.DataFrame(
+        {
+            "group_a": ["a"] * 10 + ["b"] * 10,
+            "group_b": [1, 2] * 10,
+            "yyyy_mm_dd": [f"2020-01-{d:02}" for d in range(1, 21)],
+            "y": list(range(20)),
+        }
+    )
+
+
+@pytest.fixture()
+def simple_a_b_regression_output():
+    X = np.array(
+        [[0, 2], [2, 4], [1, 3], [3, 5], [10, 12], [12, 14], [11, 13], [13, 15]]
+    )
+    y = np.array([10, 14, 12, 16, 30, 34, 32, 36])
+    group_idx = np.array(
+        [
+            "group_a=a__group_b=1",
+            "group_a=a__group_b=1",
+            "group_a=a__group_b=2",
+            "group_a=a__group_b=2",
+            "group_a=b__group_b=1",
+            "group_a=b__group_b=1",
+            "group_a=b__group_b=2",
+            "group_a=b__group_b=2",
+        ]
+    )
+    time_idx = np.array([0, 1, 0, 1, 0, 1, 0, 1])
+    return X, y, time_idx, group_idx
+
+
+@pytest.mark.parametrize("output_idx", [0, 1, 2, 3])
+def test_timeseries_regression_X(
+    simple_a_b_input_df, simple_a_b_regression_output, output_idx
+):
 
     my_timeseries = GroupedTimeSeries(
-        time_series_pd=input_df,
+        time_series_pd=simple_a_b_input_df,
         group_columns=["group_a", "group_b"],
         time_column="yyyy_mm_dd",
-        target_column="y"
+        target_column="y",
     )
 
-    out = my_timeseries.regression(
-        lookback_window=3,
-        min_predict_window=1,
-        max_predict_window=2,
-        agg_func=np.sum
+    output = my_timeseries.regression(
+        lookback_window=2, min_predict_window=1, max_predict_window=2, agg_func=np.sum
     )
 
-    # print(input_df)
-
-    # for x_i, y_i, time_id_i, group_id_i in zip(*out):
-    #     print("")
-    #     print(f"group_id = {group_id_i}")
-    #     print(f"time_id = {time_id_i}")
-    #     print(f"x_i = {x_i}")
-    #     print(f"y_i = {y_i}")
+    np.testing.assert_array_equal(
+        output[output_idx], simple_a_b_regression_output[output_idx]
+    )
