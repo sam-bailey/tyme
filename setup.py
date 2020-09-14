81dadef2183f7a4b884d536ad0337a6fff65ca21
Sam Bailey <samcbailey90@gmail.com>
2020-09-15 00:15:52 +0200

    Added and setup tox

--- setup.py
+++ setup.py
@@ -5,28 +5,25 @@
 # was previously created using Cython).
 # Set it to 'auto' to build with Cython if available, otherwise
 # from the C file.
-USE_CYTHON = 'auto'
-
-
 import sys
 
-from setuptools import setup
-from setuptools import Extension
+from setuptools import setup, find_packages, Extension
 from distutils.command.sdist import sdist as _sdist
 
 import numpy
 
-import _version
+USE_CYTHON = "auto"
 
 if USE_CYTHON:
     try:
         from Cython.Distutils import build_ext
         from Cython.Build import cythonize
     except ImportError:
-        if USE_CYTHON=='auto':
-            USE_CYTHON=False
+        if USE_CYTHON == "auto":
+            USE_CYTHON = False
         else:
             raise
+
 
 class CythonModule(object):
     def __init__(self, name: str, path: str):
@@ -51,27 +48,29 @@ class CythonModule(object):
 
     @property
     def pyx(self) -> str:
-        return self.path+".pyx"
+        return self.path + ".pyx"
 
     @property
     def c(self) -> str:
-        return self.path+".c"
+        return self.path + ".c"
+
 
 cython_modules = [
     CythonModule(
         name="tyme.base_forecasters.exponential_smoothing_cy",
-        path="src/cython/exponential_smoothing_cy"
+        path="src/cython/exponential_smoothing_cy",
     ),
     CythonModule(
         name="tyme.base_forecasters.robust_exponential_smoothing_cy",
-        path="src/cython/robust_exponential_smoothing_cy"
-    )
+        path="src/cython/robust_exponential_smoothing_cy",
+    ),
 ]
 
 if sys.version_info[0] == 2:
-    raise Exception('Python 2.x is no longer supported')
+    raise Exception("Python 2.x is no longer supported")
 
 if USE_CYTHON:
+
     class sdist(_sdist):
         def run(self):
             # Make sure the compiled Cython files in the distribution are up-to-date
@@ -79,54 +78,64 @@ if USE_CYTHON:
             _sdist.run(self)
 
     ext_modules = [
-        Extension(module.name, [module.pyx])
-        for module in cython_modules
+        Extension(module.name, [module.pyx]) for module in cython_modules
     ]
-    cmdclass = dict(
-        build_ext=build_ext,
-        sdist=sdist
-    )
+    cmdclass = dict(build_ext=build_ext, sdist=sdist)
 else:
     ext_modules = [
-        Extension(module.name, [module.c])
-        for module in cython_modules
+        Extension(module.name, [module.c]) for module in cython_modules
     ]
-    cmdclass = { }
+    cmdclass = {}
+
+requirements = [
+    "Bottleneck",
+    "cycler",
+    "kiwisolver",
+    "numpy",
+    "pandas",
+    "Pillow",
+    "pyparsing",
+    "python-dateutil",
+    "pytz",
+    "six",
+    "scipy",
+    "Cython",
+]
+
+requirements_dev = ["pytest", "Cython", "pre-commit", "tox"]
 
 setup(
-    name='tyme',
-    version=_version.__version__,
-    description='A timeseries forecasting package, specialised in forecasting grouped timeseries',
-    author='Sam Bailey',
-    author_email='samcbailey90@gmail.com',
-    url='http://github.com/sam-bailey/tyme',
-    packages=[ 'tyme'],
-    package_dir={
-        '' : 'src'
-    },
-    cmdclass = cmdclass,
+    name="tyme",
+    version="0.1.0",
+    description="A timeseries forecasting package, specialised in forecasting grouped timeseries",
+    author="Sam Bailey",
+    author_email="samcbailey90@gmail.com",
+    url="http://github.com/sam-bailey/tyme",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    cmdclass=cmdclass,
     ext_modules=ext_modules,
     include_dirs=[numpy.get_include()],
-
-    long_description=open('README.md').read(),
-
+    long_description=open("README.md").read(),
+    install_requires=requirements,
+    extras_require={"dev": requirements_dev},
     license="MIT",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Cython',
-        'Topic :: Scientific/Engineering :: Mathematics',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Cython",
+        "Topic :: Scientific/Engineering :: Mathematics",
     ],
-    keywords='timeseries forecast forecasting time',
+    keywords="timeseries forecast forecasting time",
 )
--- src/tyme/group_forecasters/optimized_hyperparameters.py
+++ src/tyme/group_forecasters/optimized_hyperparameters.py
@@ -5,7 +5,7 @@ import warnings
 from typing import Optional, List, TypeVar, Union, Tuple, Mapping, Callable
 
 from .loss_functions import LossFunctions
-from ..utils.timeseries import GroupedTimeSeries
+from ..utils import GroupedTimeSeries
 from ..base_forecasters import ExponentialSmoothing
 from ..base_forecasters import RobustExponentialSmoothing
 from ..base_forecasters.base import BaseForecaster
@@ -16,13 +16,21 @@ NumpyArray = TypeVar("numpy.ndarray")
 class GroupRegressor:
     _base_forecaster: BaseForecaster = None
 
-    def __init__(self, lookback_window: int, min_predict_window: int, max_predict_window: int,
-                 agg_func: Callable[[NumpyArray], float],
-                 loss_function: LossFunctions = LossFunctions(primary_loss="mae", monitoring_losses=["smape"]),
-                 verbose: bool = False
-                 ):
+    def __init__(
+        self,
+        lookback_window: int,
+        min_predict_window: int,
+        max_predict_window: int,
+        agg_func: Callable[[NumpyArray], float],
+        loss_function: LossFunctions = LossFunctions(
+            primary_loss="mae", monitoring_losses=["smape"]
+        ),
+        verbose: bool = False,
+    ):
 
-        assert self._base_forecaster is not None, "GroupRegressor must be subclassed and the _base_forecaster set"
+        assert (
+            self._base_forecaster is not None
+        ), "GroupRegressor must be subclassed and the _base_forecaster set"
 
         self._lookback_window = lookback_window
         self._min_predict_window = min_predict_window
@@ -32,38 +40,43 @@ class GroupRegressor:
         self._verbose = verbose
         self._fitted_base_forecaster = None
 
-    def _evaluate(self, base_forecaster, X: NumpyArray, y: NumpyArray,
-                  with_cov: bool = False, time_id: Optional[NumpyArray] = None,
-                  group_id: Optional[NumpyArray] = None
-                  ) -> Union[Tuple[float, Mapping[str, float], NumpyArray], Tuple[float, Mapping[str, float]]]:
-
+    def _evaluate(
+        self,
+        base_forecaster,
+        X: NumpyArray,
+        y: NumpyArray,
+        with_cov: bool = False,
+        time_id: Optional[NumpyArray] = None,
+        group_id: Optional[NumpyArray] = None,
+    ) -> Union[
+        Tuple[float, Mapping[str, float], NumpyArray],
+        Tuple[float, Mapping[str, float]],
+    ]:
         def _filter_forecast_aggregate(x_history: NumpyArray) -> float:
             base_forecaster.filter(x_history)
             _fcst = base_forecaster.forecast(
-                n_steps_min = self._min_predict_window,
-                n_steps_max = self._max_predict_window
+                n_steps_min=self._min_predict_window,
+                n_steps_max=self._max_predict_window,
             )
             return self._agg_func(_fcst)
 
         y_pred = np.array(list(map(_filter_forecast_aggregate, X)))
         loss = self._loss.primary_loss(y_pred=y_pred, y_true=y)
-        monitoring_losses = self._loss.monitoring_losses(y_pred=y_pred, y_true=y)
+        monitoring_losses = self._loss.monitoring_losses(
+            y_pred=y_pred, y_true=y
+        )
 
         if with_cov:
-            df1 = pd.DataFrame({
-                "group_id": group_id,
-                "time_id": time_id,
-                "err": y_pred - y
-            })
-            df2 = pd.DataFrame({
-                "group_id": group_id,
-                "time_id": time_id,
-                "err": y_pred - y
-            })
+            df1 = pd.DataFrame(
+                {"group_id": group_id, "time_id": time_id, "err": y_pred - y}
+            )
+            df2 = pd.DataFrame(
+                {"group_id": group_id, "time_id": time_id, "err": y_pred - y}
+            )
             cov_df = (
                 df1.merge(df2, on="time_id")
                 .groupby(["group_id_x", "group_id_y"])
-                .apply(lambda x: x['err_x'].cov(x['err_y']))
+                .apply(lambda x: x["err_x"].cov(x["err_y"]))
                 .reset_index()
             )
 
@@ -71,29 +84,37 @@ class GroupRegressor:
         else:
             return loss, monitoring_losses
 
-    def fit(self, timeseries: GroupedTimeSeries, optimizer: str ="L-BFGS-B",
-            starting_params: Optional[List[float]] = None) -> None:
+    def fit(
+        self,
+        timeseries: GroupedTimeSeries,
+        optimizer: str = "L-BFGS-B",
+        starting_params: Optional[List[float]] = None,
+    ) -> None:
 
         X, y, _, _ = timeseries.regression(
             lookback_window=self._lookback_window,
             min_predict_window=self._min_predict_window,
             max_predict_window=self._max_predict_window,
-            agg_func=self._agg_func
+            agg_func=self._agg_func,
         )
 
         if starting_params is None:
-            starting_params = self._base_forecaster.get_default_starting_params()
+            starting_params = (
+                self._base_forecaster.get_default_starting_params()
+            )
         param_bounds = self._base_forecaster.get_param_bounds()
 
         def _minimize_me(params_lst, info):
             base_forecaster = self._base_forecaster.create_from_lst(params_lst)
             loss, monitoring_losses = self._evaluate(base_forecaster, X, y)
 
-            if (info['Nfeval'] % 10 == 0) & self._verbose:
-                print('Params={0}  Loss={1:9f}   Monitoring Losses={2}'.format(
-                    str(params_lst), loss, str(monitoring_losses))
+            if (info["Nfeval"] % 10 == 0) & self._verbose:
+                print(
+                    "Params={0}  Loss={1:9f}   Monitoring Losses={2}".format(
+                        str(params_lst), loss, str(monitoring_losses)
+                    )
                 )
-            info['Nfeval'] += 1
+            info["Nfeval"] += 1
 
             return loss
 
@@ -102,32 +123,49 @@ class GroupRegressor:
             x0=starting_params,
             bounds=param_bounds,
             method=optimizer,
-            args=({'Nfeval': 0},)
+            args=({"Nfeval": 0},),
         )
-        self._fitted_base_forecaster = self._base_forecaster.create_from_lst(self._fit_results.x)
+        self._fitted_base_forecaster = self._base_forecaster.create_from_lst(
+            self._fit_results.x
+        )
 
     def forecast(self, timeseries: GroupedTimeSeries) -> pd.DataFrame:
         pred_col_name = "{}_forecast".format(timeseries._target_column)
 
-        out_pdf = timeseries._raw_pdf[[
-            timeseries._time_column, timeseries._target_column, timeseries._group_id_column
-        ]]
+        out_pdf = timeseries._raw_pdf[
+            [
+                timeseries._time_column,
+                timeseries._target_column,
+                timeseries._group_id_column,
+            ]
+        ]
 
         out_pdf.loc[pred_col_name] = None
         out_lst = [out_pdf]
 
-        for group_id, pdf_i in timeseries._raw_pdf.groupby(timeseries._group_id_column):
+        for group_id, pdf_i in timeseries._raw_pdf.groupby(
+            timeseries._group_id_column
+        ):
             time_max = pdf_i[timeseries._time_column].values[-1]
 
-            y = pdf_i[timeseries._target_column].values[-self._lookback_window:]
+            y = pdf_i[timeseries._target_column].values[
+                -self._lookback_window :
+            ]
             self._fitted_base_forecaster.filter(y)
-            forecast_y = self._fitted_base_forecaster.forecast(1, self._max_predict_window)
-            forecast_time = [time_max + timeseries._frequency*(n+1.0) for n in range(self._max_predict_window)]
+            forecast_y = self._fitted_base_forecaster.forecast(
+                1, self._max_predict_window
+            )
+            forecast_time = [
+                time_max + timeseries._frequency * (n + 1.0)
+                for n in range(self._max_predict_window)
+            ]
 
-            pdf_out_i = pd.DataFrame({
-                timeseries._time_column: forecast_time,
-                pred_col_name: forecast_y
-            })
+            pdf_out_i = pd.DataFrame(
+                {
+                    timeseries._time_column: forecast_time,
+                    pred_col_name: forecast_y,
+                }
+            )
             pdf_out_i[timeseries._target_column] = None
             pdf_out_i[timeseries._group_id_column] = group_id
 
@@ -141,20 +179,17 @@ class GroupRegressor:
         def _filter_forecast_aggregate(x_history):
             self._fitted_base_forecaster.filter(x_history.values)
             _fcst = self._fitted_base_forecaster.forecast(
-                n_steps_min = self._min_predict_window,
-                n_steps_max = self._max_predict_window
+                n_steps_min=self._min_predict_window,
+                n_steps_max=self._max_predict_window,
             )
             return self._agg_func(_fcst)
 
         output_pdf = (
-            timeseries._raw_pdf
-            .sort_values(timeseries._time_column)
+            timeseries._raw_pdf.sort_values(timeseries._time_column)
             .groupby(timeseries._group_id_column)
-            .agg({
-                timeseries._target_column: _filter_forecast_aggregate
-            })
+            .agg({timeseries._target_column: _filter_forecast_aggregate})
             .reset_index()
-            .rename({timeseries._target_column: pred_col_name}, axis='columns')
+            .rename({timeseries._target_column: pred_col_name}, axis="columns")
         )
 
         return output_pdf
@@ -164,10 +199,16 @@ class GroupRegressor:
             lookback_window=self._lookback_window,
             min_predict_window=self._min_predict_window,
             max_predict_window=self._max_predict_window,
-            agg_func=self._agg_func
+            agg_func=self._agg_func,
         )
-        return self._evaluate(self._fitted_base_forecaster, X, y,
-                              with_cov=with_cov, time_id=time_id, group_id=group_id)
+        return self._evaluate(
+            self._fitted_base_forecaster,
+            X,
+            y,
+            with_cov=with_cov,
+            time_id=time_id,
+            group_id=group_id,
+        )
 
 
 class RobustExponentialSmoothingGroupRegressor(GroupRegressor):
